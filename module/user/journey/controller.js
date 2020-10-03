476e4fba85960747d1fb6f04baad5722e37b0667
Harsh Kesharwani <harsh.kesharwani@biz2credit.org>
2020-10-04 00:15:37 +0530

    Save contacts API

--- module/user/journey/controller.js
+++ module/user/journey/controller.js
@@ -5,32 +5,41 @@ const Contacts = require('./contacts-model');
 module.exports.saveContact = async function (req, res, next) {
     try {
         let user_id = req.headers.id;
-        if (Array.isArray(req.body)) {
-            let a = []
-            req.body.forEach(element => {
+        if(!Array.isArray(req.body)) sendResponse(res, false, 400, 6002)
 
+        let bulkWriteQuery = req.body.map(elem => {
+            elem['is_active'] = false;
+            return {
+                updateOne: {
+                    "filter": { mobile: elem.mobile },
+                    "update": { $setOnInsert: elem },
+                    "upsert": true
+                }
+            }
+        });
 
-            });
+        await User.bulkWrite(bulkWriteQuery);
+        let mobile_nos = [];
+        req.body.map(el => mobile_nos.push(el.mobile));
+        let contactsResult = await User.find({ "mobile": { "$in": mobile_nos } }, { _id: 1 });
+        let contactIds = [];
+        contactsResult.forEach(element => contactIds.push(element._id));
+        await Contacts.updateOne({user_id : user_id}, {$set : {user_id : user_id, contact_list : contactIds}}, {upsert : true});
 
-        }
-
-        sendResponse(res, false, 200, 3007)
+        sendResponse(res, false, 200, 3007, contactsResult)
     } catch (err) {
         console.log(err)
-
+        sendResponse(res, false, 403, 6001, err)
     }
 
 }
 
-module.exports.userDetails = function (req, res, next) {
+module.exports.userDetails = async function (req, res, next) {
     try {
-        let { mobile_no } = req.body;
-        console.log('Get user Details', mobile_no);
-
-
-        sendResponse(res, false, 200, 3007)
+        let user_id = req.headers.id;
+        let userDetails = await User.find({_id : user_id});
+        sendResponse(res, false, 200, 3008, userDetails);
     } catch (err) {
-
+        sendResponse(res, false, 403, 6001, err);
     }
-
 }
